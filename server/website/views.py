b65bb2cab51f795057603382a90ba3ce2d3cdbe2
Kevin Yven Riexinger <info@kevinriexinger.de>
2023-04-12 22:15:29 +0200

    server

--- server/website/views.py
+++ server/website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    if request.POST:
+        file = request.POST.FILES
+        print(file)
+    return HttpResponse("Hello World;")
